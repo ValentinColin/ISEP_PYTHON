5cf65123a2c52e6de89d1e70c856cafa33b72c85
Valentin <45980985+ValentinColin@users.noreply.github.com>
2019-03-18 22:20:00 +0100

    Add files via upload

---
+++ fonction_Tri.py
@@ -0,0 +1,16 @@
+# --coding:utf-8--
+def tri(listeNT=[]):
+    """prend une liste de nombre ENTIER en argument et renvoie la liste triée"""
+    for elem in listeNT:
+        if type(elem) is not int:
+            print("TypeError: la liste donner en arguments n'est pas une liste de nombre")
+            return listeNT #renvoie la liste non modifié en cas d'erreur
+    listeT=[]
+    for k in range(len(listeNT)):#je fais len(listeNT) boucle pour retirer le plus petit à chaque fois
+        min=listeNT[0]
+        for elem in listeNT:# je parcours les elem restant
+            if elem<=min:   # pour voir si je trouve un nombre encore plus petit
+                min=elem
+        listeT.append(min)
+        del listeNT[listeNT.index(min)]
+    return listeT # renvoie la liste trier
